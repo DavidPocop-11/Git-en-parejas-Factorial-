44081938076ec6caf444ba38797500600d41a9b3
David Pocop <davidalexanderpocopcuxulic@gmail.com>
2026-05-13 20:53:45 -0600

    Agregar archivos de proyecto.

---
+++ Factorial-/Factorial-.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include<iostream>
+using namespace std;
+int factorial(int num);
+int numero;
+
+void main() {
+	int numero;
+	cout << "Numero para calcular factorial";
+	cin >> numero;
+	cout << "El factoriales" << factorial(numero);
+}
+
